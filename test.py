1f68b1c5d9498802d6c1f6068988b1271580df0d
Jesus M. Gonzalez-Barahona <jgb@gsyc.es>
2014-02-20 00:29:19 +0100

    Some more testing...

--- test.py
+++ test.py
@@ -30,14 +30,47 @@ def loadSession():
     session = Session()
     return session
 
-def getCommitsQuery (start, end):
+# class VarType:
+#     """Class for specifying the kind of variable
+
+#     Kinds of variables of interest are aggregate, count, list,
+#     time series...
+#     """
+
+#     def selector(self, field):
+#         return (selector)
+
+#     def __init__(self, selector):
+#         self.selector = selector
+
+# VarTypeCount = VarType (func.count)
+
+# def getCommitsQuery (type, start, end):
+#     """
+#     Get commits
+
+#     Get all commits between starting date and end date
+#       (exactly: start <= date < end
+#     - selected: what is in the SELECt statement for the query,
+#         such as func.count(func.distinct(SCMLog.id)
+#     - start: string, starting date, such as "2013-06-01"
+#     - end: string, end date, such as "2014-01-01"
+#     """
+
+#     res = session.query(type.selector(func.distinct(SCMLog.id))).\
+#         join(Actions).\
+#         filter(SCMLog.date >= start).\
+#         filter(SCMLog.date < end)
+#     return res
+
+def getNCommitsQuery (start, end):
     """
     Get commits
 
     Get all commits between starting date and end date
       (exactly: start <= date < end
-    - Start: string, starting date, such as "2013-06-01"
-    - End: string, end date, such as "2014-01-01"
+    - start: string, starting date, such as "2013-06-01"
+    - end: string, end date, such as "2014-01-01"
 
 # SELECT count(distinct(s.id)) as commits
 # FROM scmlog s, actions a
@@ -47,6 +80,22 @@ def getCommitsQuery (start, end):
 """
 
     res = session.query(func.count(func.distinct(SCMLog.id))).\
+        join(Actions).\
+        filter(SCMLog.date >= start).\
+        filter(SCMLog.date < end)
+    return res
+
+def getCommitsQuery (start, end):
+    """
+    Get commits
+
+    Get all commits between starting date and end date
+      (exactly: start <= date < end
+    - start: string, starting date, such as "2013-06-01"
+    - end: string, end date, such as "2014-01-01"
+"""
+
+    res = session.query(func.distinct(SCMLog.id).label("id"), SCMLog.date).\
         join(Actions).\
         filter(SCMLog.date >= start).\
         filter(SCMLog.date < end)
@@ -62,6 +111,22 @@ if __name__ == "__main__":
     print
     res = session.query(Actions.id).distinct(Actions.type)
     print res.first()
-    
+
+    # print "Other"
+    # print
+    # res = getCommitsQuery(selected=func.count(func.distinct(SCMLog.id)),
+    #                       start="2013-06-01", end="2014-01-01")
+    # print res.first()
+
+    print "Other"
+    print
+#    res = getCommitsQuery(selected=func.count(SCMLog.id),
+#                          start="2013-06-01", end="2014-01-01")
+#    res = getCommitsQuery(type=VarTypeCount,
+#                          start="2013-06-01", end="2014-01-01")
+    res = getNCommitsQuery(start="2013-06-01", end="2014-01-01")
+    print res.scalar()
     res = getCommitsQuery(start="2013-06-01", end="2014-01-01")
-    print res.first()
+    print res.column_descriptions
+    for row in res.limit(10).all():
+        print row.id, row.date
