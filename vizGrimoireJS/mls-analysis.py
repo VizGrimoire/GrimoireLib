7ba8a9840b9e14246b963da65bcb5bfcfac570b2
Alvaro del Castillo <acs@bitergia.com>
2014-03-14 16:12:58 +0100

    MLS cleaned from not needed R code.

--- vizGrimoireJS/mls-analysis.py
+++ vizGrimoireJS/mls-analysis.py
@@ -44,8 +44,6 @@ from GrimoireUtils import valRtoPython, read_options, getPeriod
 import MLS
 
 def aggData(period, startdate, enddate, identities_db, destdir):
-#    data = vizr.StaticMLSInfo(period, startdate, enddate, identities_db, rfield)
-#    agg = dataFrame2Dict(data)
     data = MLS.StaticMLSInfo(period, startdate, enddate, identities_db, rfield)
     agg = data
 
@@ -64,7 +62,6 @@ def aggData(period, startdate, enddate, identities_db, destdir):
 
     # Tendencies
     for i in [7,30,365]:
-        # period_data = dataFrame2Dict(vizr.GetDiffSentDays(period, enddate, i))
         period_data = MLS.GetDiffSentDays(period, enddate, i)
         agg = dict(agg.items() + period_data.items())
         period_data = MLS.GetDiffSendersDays(period, enddate, i)
@@ -80,8 +77,6 @@ def aggData(period, startdate, enddate, identities_db, destdir):
 def tsData(period, startdate, enddate, identities_db, destdir, granularity, conf):
 
     evol = {}
-#    data = vizr.EvolMLSInfo(period, startdate, enddate, identities_db, rfield)
-#    evol = dict(evol.items() + completePeriodIds(dataFrame2Dict(data)).items())
     data = MLS.EvolMLSInfo(period, startdate, enddate, identities_db, rfield)
     evol = dict(evol.items() + completePeriodIds(data).items())
 
@@ -107,8 +102,7 @@ def peopleData(period, startdate, enddate, identities_db, destdir, top_data):
     top += top_data['senders.last month']["id"]
     # remove duplicates
     people = list(set(top))
-    # the order is not the same than in R json
-    createJSON(people, destdir+"/mls-people.json", False)
+    createJSON(people, destdir+"/mls-people.json")
 
     for upeople_id in people:
         evol = MLS.GetEvolPeopleMLS(upeople_id, period, startdate, enddate)
@@ -126,10 +120,9 @@ def reposData(period, startdate, enddate, identities_db, destdir, conf, repofiel
     check = True
     if not isinstance(repos, (list)):
         repos = [repos]
-        check = False
     repos_files = [repo.replace('/', '_').replace("<","__").replace(">","___")
             for repo in repos]
-    createJSON(repos_files, destdir+"/mls-repos.json", check)
+    createJSON(repos_files, destdir+"/mls-repos.json")
 
     for repo in repos:
         # Evol data   
@@ -144,7 +137,7 @@ def reposData(period, startdate, enddate, identities_db, destdir, conf, repofiel
         createJSON (data, destdir+"/"+listname_file+"-mls-rep-evolutionary.json")
 
         top_senders = MLS.repoTopSenders (repo, identities_db, startdate, enddate, repofield, npeople)
-        createJSON(top_senders, destdir+ "/"+listname_file+"-mls-rep-top-senders.json", False)
+        createJSON(top_senders, destdir+ "/"+listname_file+"-mls-rep-top-senders.json")
 
         # Static data
         data = MLS.StaticMLSInfo(period, startdate, enddate, identities_db, rfield, ["repository", repo_name])
@@ -154,7 +147,6 @@ def reposData(period, startdate, enddate, identities_db, destdir, conf, repofiel
         createJSON (data, destdir+ "/"+listname_file+"-mls-rep-static.json")
 
 def companiesData(period, startdate, enddate, identities_db, destdir, npeople):
-    # companies = valRtoPython(vizr.companiesNames(identities_db, startdate, enddate))
     companies = MLS.companiesNames(identities_db, startdate, enddate)
     createJSON(companies, destdir+"/mls-companies.json")
 
@@ -162,14 +154,10 @@ def companiesData(period, startdate, enddate, identities_db, destdir, npeople):
         company_name = "'"+company+ "'"
         data = MLS.EvolMLSInfo(period, startdate, enddate, identities_db, rfield, ["company", company_name])
         data = completePeriodIds(data)
-        if company in ["company4","Deutsche Telekom","IBM"]:
-            # Wrong JSON generated in R. Don't check
-            createJSON(data, destdir+"/"+company+"-mls-com-evolutionary.json", False)
-        else:
-            createJSON(data, destdir+"/"+company+"-mls-com-evolutionary.json")
+        createJSON(data, destdir+"/"+company+"-mls-com-evolutionary.json")
 
         top_senders = MLS.companyTopSenders (company, identities_db, startdate, enddate, npeople)
-        createJSON(top_senders, destdir+"/"+company+"-mls-com-top-senders.json", False)
+        createJSON(top_senders, destdir+"/"+company+"-mls-com-top-senders.json")
 
         data = MLS.StaticMLSInfo(period, startdate, enddate, identities_db, rfield, ["company", company_name])
         createJSON(data, destdir+"/"+company+"-mls-com-static.json")
@@ -187,11 +175,7 @@ def countriesData(period, startdate, enddate, identities_db, destdir, npeople):
         type_analysis = ["country", country_name]
         data = MLS.EvolMLSInfo(period, startdate, enddate, identities_db, rfield, type_analysis)
         data = completePeriodIds(data)
-        if (country == "country5" or country == "country2"):
-            # Wrong JSON generated in R. Don't check
-            createJSON(data, destdir+"/"+country+"-mls-cou-evolutionary.json", False)
-        else:
-            createJSON (data, destdir+"/"+country+"-mls-cou-evolutionary.json")
+        createJSON (data, destdir+"/"+country+"-mls-cou-evolutionary.json")
 
         top_senders = MLS.countryTopSenders (country, identities_db, startdate, enddate, npeople)
         createJSON(top_senders, destdir+"/"+country+"-mls-cou-top-senders.json")
@@ -209,14 +193,10 @@ def domainsData(period, startdate, enddate, identities_db, destdir, npeople):
         type_analysis = ["domain", domain_name]
         data = MLS.EvolMLSInfo(period, startdate, enddate, identities_db, rfield, type_analysis)
         data = completePeriodIds(data)
-        if (domain == "everybody" or domain == "hallowelt"):
-            # Wrong JSON generated in R. Don't check
-            createJSON(data, destdir+"/"+domain+"-mls-dom-evolutionary.json", False)
-        else:
-            createJSON(data, destdir+"/"+domain+"-mls-dom-evolutionary.json")
+        createJSON(data, destdir+"/"+domain+"-mls-dom-evolutionary.json")
 
         data = MLS.domainTopSenders(domain, identities_db, startdate, enddate, npeople)
-        createJSON(data, destdir+"/"+domain+"-mls-dom-top-senders.json", False)
+        createJSON(data, destdir+"/"+domain+"-mls-dom-top-senders.json")
 
         data = MLS.StaticMLSInfo(period, startdate, enddate, identities_db, rfield, type_analysis)
         createJSON(data, destdir+"/"+domain+"-mls-dom-static.json")
@@ -227,15 +207,15 @@ def topData(period, startdate, enddate, identities_db, destdir, bots, npeople):
     top_senders_data['senders.last year']=MLS.top_senders(365, startdate, enddate,identities_db, bots, npeople)
     top_senders_data['senders.last month']=MLS.top_senders(31, startdate, enddate,identities_db,bots, npeople)
 
-    createJSON (top_senders_data, destdir+"/mls-top.json", False)
+    createJSON (top_senders_data, destdir+"/mls-top.json")
 
     return top_senders_data
 
-def demographics(enddate, destdir):
+def microstudies(enddate, destdir):
+
     vizr.ReportDemographicsAgingMLS(enddate, destdir)
     vizr.ReportDemographicsBirthMLS(enddate, destdir)
 
-def timeToAttend(destdir):
     ## Which quantiles we're interested in
     quantiles_spec = [0.99,0.95,0.5,0.25]
 
@@ -244,20 +224,21 @@ def timeToAttend(destdir):
     ## JSON files generated from VizR
     vizr.ReportTimeToAttendMLS(destdir)
 
-
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO,format='%(asctime)s %(message)s')
     logging.info("Starting MLS data source analysis")
     opts = read_options()
     period = getPeriod(opts.granularity)
     reports = opts.reports.split(",")
+
     # filtered bots
+    bots = ['wikibugs','gerrit-wm','wikibugs_','wm-bot','','Translation updater bot','jenkins-bot']
 
     # Working at the same time with VizR and VizPy yet
     vizr.SetDBChannel (database=opts.dbname, user=opts.dbuser, password=opts.dbpassword)
     GrimoireSQL.SetDBChannel (database=opts.dbname, user=opts.dbuser, password=opts.dbpassword)
 
-    bots = ['wikibugs','gerrit-wm','wikibugs_','wm-bot','','Translation updater bot','jenkins-bot']
+
     # TODO: hack because VizR library needs. Fix in lib in future
     startdate = "'"+opts.startdate+"'"
     enddate = "'"+opts.enddate+"'"
@@ -282,7 +263,6 @@ if __name__ == '__main__':
 
 
     # R specific reports
-    demographics(opts.enddate, opts.destdir)
-    timeToAttend(opts.destdir)
+    microstudies(opts.enddate, opts.destdir)
 
     logging.info("MLS data source analysis OK")
