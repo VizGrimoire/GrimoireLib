69fe537bb7cb896f2e01bea66c335279f069ba38
Alvaro del Castillo <acs@bitergia.com>
2014-03-17 18:48:33 +0100

    report.py is the report module. report_tool.py the tool that uses it.

--- vizGrimoireJS/report_tool.py
+++ vizGrimoireJS/report_tool.py
@@ -31,19 +31,28 @@ from GrimoireUtils import read_options, getPeriod, read_main_conf
 from SCM import GetPeopleListSCM
 from data_source import DataSource
 
+# All data sources
+import SCM, ITS, MLS, SCR, Mediawiki, IRC
+data_sources = [SCM.SCM, ITS.ITS, MLS.MLS, SCR.SCR, Mediawiki.Mediawiki, IRC.IRC]
+
 def get_evol_report(startdate, enddate, identities_db, bots, type_analysis):
-    # TODO: DRY this options
-
     all_ds = {}
-
-    import SCM, ITS, MLS, SCR, Mediawiki, IRC
-    data_sources = [SCM.SCM, ITS.ITS, MLS.MLS, SCR.SCR, Mediawiki.Mediawiki, IRC.IRC]
 
     for ds in data_sources:
         if not ds.get_db_name() in automator['generic']: continue
         db = automator['generic'][ds.get_db_name()]
         GrimoireSQL.SetDBChannel (database=db, user=opts.dbuser, password=opts.dbpassword)
         all_ds[ds.get_name()] = ds.get_evolutionary_data (period, startdate, enddate, identities_db, type_analysis)
+    return all_ds
+
+def get_agg_report(startdate, enddate, identities_db, bots, type_analysis):
+    all_ds = {}
+
+    for ds in data_sources:
+        if not ds.get_db_name() in automator['generic']: continue
+        db = automator['generic'][ds.get_db_name()]
+        GrimoireSQL.SetDBChannel (database=db, user=opts.dbuser, password=opts.dbpassword)
+        all_ds[ds.get_name()] = ds.get_agg_data (period, startdate, enddate, identities_db, type_analysis)
     return all_ds
 
 
@@ -64,5 +73,6 @@ if __name__ == '__main__':
     opts.config_file = "../../../conf/main.conf"
     automator = read_main_conf(opts.config_file)
     print(get_evol_report(startdate, enddate, opts.identities_db, [], []))
+    # print(get_agg_report(startdate, enddate, opts.identities_db, [], []))
 
     logging.info("Report data source analysis OK")
