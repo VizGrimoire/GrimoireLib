95d0839ad90093da7357b2e50b52d2212d4f7ca3
Alvaro del Castillo <acs@bitergia.com>
2014-03-22 12:18:51 +0100

    First testing for Data Source API.

---
+++ testing/test_data_source_api.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2013 Bitergia
+#
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+#
+# Authors:
+#   Alvaro del Castillo San Felix <acs@bitergia.com>
+#
+# import MySQLdb, os, random, string, sys
+# import data_source
+
+import unittest
+from report import Report
+import logging
+
+class DataSourceTest(unittest.TestCase):
+    @staticmethod
+    def init():
+        Report.init()
+        logging.info("Init Data Source")
+
+    @staticmethod
+    def close():
+        logging.info("End Data Source")
+
+    def test_get_bots(self):
+        for ds in Report.get_data_sources():
+            bots = ds.get_bots()
+            self.assertTrue(isinstance(bots, list))
+
+    def test_set_bots(self):
+        bots = ['test']
+        for ds in Report.get_data_sources():
+            ds.set_bots(bots)
+            ds_bots = ds.get_bots()
+            self.assertEqual(ds_bots[0], bots[0])
+
+    def test_get_name(self):
+        # Run the same tests over all data sources
+        for ds in Report.get_data_sources():
+            ds_name = ds.get_name()
+            self.assertNotEqual(ds_name, "IRC")
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO,format='%(asctime)s %(message)s')
+
+    DataSourceTest.init()
+    suite = unittest.TestLoader().loadTestsFromTestCase(DataSourceTest)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+    DataSourceTest.close()
