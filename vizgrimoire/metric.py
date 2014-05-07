03bd3fe7133e9eb7820c25cb7cc8ee63f704bfd2
Alvaro del Castillo <acs@bitergia.com>
2014-05-07 08:54:56 +0200

    First steps towards a metrics using classes approach.

---
+++ vizgrimoire/metric.py
@@ -0,0 +1,32 @@
+## Copyright (C) 2014 Bitergia
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+## GNU General Public License for more details. 
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+##
+## This file is a part of GrimoireLib
+##  (an Python library for the MetricsGrimoire and vizGrimoire systems)
+##
+##
+## Authors:
+##   Alvaro del Castillo <acs@bitergia.com>
+
+""" Metric offers the API supported by all metrics """
+
+class Metric(object):
+
+    def __init__(self, id, name = None, desc = None, data_source = None):
+        self.id = id
+        self.name = name
+        self.desc = desc
+        self.data_source = data_source
