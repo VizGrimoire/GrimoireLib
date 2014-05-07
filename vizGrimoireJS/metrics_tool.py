2a7f7b3e5e3b32a3a8f1cdc51cbba88b8c603306
Alvaro del Castillo <acs@bitergia.com>
2014-05-07 08:56:02 +0200

    Sample of creating Commit metric.

--- vizGrimoireJS/metrics_tool.py
+++ vizGrimoireJS/metrics_tool.py
@@ -53,3 +53,5 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO,format='%(asctime)s %(message)s')
     logging.info("Grimoire Metrics Tool")
     opts = get_options()
+
+    commit = Commit('')
