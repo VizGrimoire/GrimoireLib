7e22ebfe313cf98c3dfeefb62f4c9f839c31a5af
Alvaro del Castillo <acs@bitergia.com>
2014-06-14 07:06:13 +0200

    Update identifiers creation API.

--- vizGrimoireJS/report_tool.py
+++ vizGrimoireJS/report_tool.py
@@ -264,7 +264,7 @@ if __name__ == '__main__':
         create_reports_filters(period, startdate, enddate, opts.destdir, opts.npeople, identities_db, bots)
     if not opts.filter:
         create_reports_studies(period, startdate, enddate, opts.destdir)
-    create_people_identifiers(startdate, enddate, opts.destdir, identities_db, bots)
+    create_people_identifiers(startdate, enddate, opts.destdir)
 
 
     logging.info("Report data source analysis OK")
