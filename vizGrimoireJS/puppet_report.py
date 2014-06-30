e2f913965afa4a98d1f458203f0e9e997a3dee88
Luis Cañas-Díaz <lcanas@bitergia.com>
2014-06-30 20:35:10 +0200

    [example][puppet] Add onion transitions study to script

--- vizGrimoireJS/puppet_report.py
+++ vizGrimoireJS/puppet_report.py
@@ -155,6 +155,16 @@ def scm_report(dbcon, filters):
     createJSON(top_repos, "./release/scm_top_repositories.json")
     createCSV(top_repos, "./release/scm_top_repositories.csv")
 
+    from onion_transitions import OnionTransitions
+    ot = OnionTransitions(scm_dbcon, filters)
+    ot_data = ot.result()
+    #= {"up_core":up_core, "up_reg":up_reg, "down_reg":down_reg, "down_occ":down_occ}
+    createCSV(ot_data["up_reg"], "./release/scm_occasional_to_regular.csv")
+    createCSV(ot_data["up_core"], "./release/scm_regular_to_core.csv")
+    createCSV(ot_data["down_occ"], "./release/scm_regular_to_occasional.csv")
+    createCSV(ot_data["down_reg"], "./release/scm_core_to_regular.csv")
+
+
 def qaforums_report(dbcon, filters):
     questions = qa.Questions(dbcon, filters)
     createJSON(questions.get_agg(), "./release/qaforums_questions.json")
