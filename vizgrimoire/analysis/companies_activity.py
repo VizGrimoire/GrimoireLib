f88f65a15f7a72dca06793d6e4dd7a4e5e1a4f8c
Alvaro del Castillo <acs@bitergia.com>
2014-07-01 08:08:23 +0200

    [companies activity] Implemented metrics from ITS and MLS also.

--- vizgrimoire/analysis/companies_activity.py
+++ vizgrimoire/analysis/companies_activity.py
@@ -29,7 +29,10 @@ from analyses import Analyses
 from query_builder import DSQuery
 from metrics_filter import MetricFilters
 from GrimoireUtils import createJSON
+import GrimoireSQL
 from SCM import SCM
+from ITS import ITS
+from MLS import MLS
 from report import Report
 
 class CompaniesActivity(Analyses):
@@ -37,20 +40,45 @@ class CompaniesActivity(Analyses):
     name = "Companies Activity"
     desc = "Companies activity for different periods: commits, authors, actions ..."
 
-    def get_from_companies(self):
+    def get_mls_from_companies(self):
+        automator = Report.get_config()
+        identities_db = automator['generic']['db_identities']
+
+        from_ = """
+            FROM messages m
+              JOIN messages_people mp ON m.message_ID = mp.message_id
+              JOIN people_upeople pup ON mp.email_address = pup.people_id 
+              JOIN %s.upeople_companies upc ON upc.upeople_id = pup.upeople_id 
+              JOIN %s.companies c ON c.id = upc.company_id 
+        """ % (identities_db, identities_db)
+        return from_
+
+
+    def get_its_from_companies(self):
+        automator = Report.get_config()
+        identities_db = automator['generic']['db_identities']
+
+        from_ = """
+            FROM issues i
+              JOIN people_upeople pup ON i.submitted_by = pup.people_id 
+              JOIN %s.upeople_companies upc ON upc.upeople_id = pup.upeople_id 
+              JOIN %s.companies c ON c.id = upc.company_id 
+        """ % (identities_db, identities_db)
+        return from_
+
+    def get_scm_from_companies(self):
         from_ = """
             FROM scmlog s 
               JOIN people_upeople pup ON s.author_id = pup.people_id 
               JOIN upeople_companies upc ON upc.upeople_id = pup.upeople_id 
               JOIN companies c ON c.id = upc.company_id 
-              JOIN people p ON p.id = s.author_id
         """
         return from_
 
     def get_sql_commits(self, year = None):
         where = ""
         field = "commits"
-        from_ =  self.get_from_companies()
+        from_ =  self.get_scm_from_companies()
 
         if year is not None:
             where = " WHERE YEAR(s.date) = " + str(year)
@@ -59,7 +87,7 @@ class CompaniesActivity(Analyses):
             select c.name, count(c.id) as %s
             %s %s
             group by c.id
-            order by %s desc
+            order by %s desc, c.name
         """ % (field, from_, where, field)
 
         return (sql)
@@ -67,7 +95,7 @@ class CompaniesActivity(Analyses):
     def get_sql_authors(self, year = None):
         where = ""
         field = "authors"
-        from_ =  self.get_from_companies()
+        from_ =  self.get_scm_from_companies()
 
         if year is not None:
             where = " WHERE YEAR(s.date) = " + str(year)
@@ -76,7 +104,7 @@ class CompaniesActivity(Analyses):
             select c.name, count(distinct(s.author_id)) as %s
             %s %s
             group by c.id
-            order by %s desc
+            order by %s desc, c.name
         """ % (field, from_, where, field)
 
         return (sql)
@@ -84,7 +112,7 @@ class CompaniesActivity(Analyses):
     def get_sql_actions(self, year = None):
         where = ""
         field = "actions"
-        from_ =  self.get_from_companies()
+        from_ =  self.get_scm_from_companies()
 
         if year is not None:
             where = " WHERE YEAR(s.date) = " + str(year)
@@ -95,11 +123,59 @@ class CompaniesActivity(Analyses):
               JOIN actions a ON a.commit_id = s.id 
             %s
             group by c.id
-            order by %s desc
+            order by %s desc, c.name
         """ % (field, from_, where, field)
 
         return (sql)
 
+    def get_sql_tickets(self, field = None, year = None):
+        where = "WHERE"
+        from_ =  self.get_its_from_companies()
+        if field == "opened":
+            if year is None: where = ""
+        elif field == "closed":
+            where = "WHERE i.status='RESOLVED' "
+        elif field == "pending":
+            where = "WHERE i.status='NEW' "
+        else: return
+
+        if year is not None:
+            if where != "WHERE": where += " AND "
+            where += " YEAR(i.submitted_on) = " + str(year) 
+            field = field + "_" + str(year)
+        sql = """
+            select c.name, count(distinct(i.id)) as %s
+            %s %s
+            group by c.id
+            order by %s desc, c.name
+        """ % (field, from_, where, field)
+        return (sql)
+
+    def get_sql_opened(self, year = None):
+        return self.get_sql_tickets("opened", year)
+
+    def get_sql_closed(self, year = None):
+        return self.get_sql_tickets("closed", year)
+
+    def get_sql_pending(self, year = None):
+        return self.get_sql_tickets("pending", year)
+
+    def get_sql_sent(self, year = None):
+        where = ""
+        field = "sent"
+        from_ =  self.get_mls_from_companies()
+
+        if year is not None:
+            where = " WHERE YEAR(m.first_date) = " + str(year)
+            field = field + "_" + str(year)
+
+        sql = """
+            select c.name, count(distinct(m.message_ID)) as %s
+            %s %s
+            group by c.id
+            order by %s desc, c.name
+        """ % (field, from_, where, field)
+        return (sql)
 
     def create_report(self, data_source, destdir):
         if data_source != SCM: return
@@ -130,7 +206,7 @@ class CompaniesActivity(Analyses):
             if not isinstance(data[item], list): data[item] = [data[item]]
 
     def add_metric_years(self, metric, activity, start, end):
-        metrics = ['commits','authors','actions']
+        metrics = ['commits','authors','actions','opened','closed','sent']
         if metric not in metrics:
             logging.error(metric + " not supported in companies activity.")
             return
@@ -141,13 +217,23 @@ class CompaniesActivity(Analyses):
                 data = self.db.ExecuteQuery(self.get_sql_authors(i))
             elif metric == "actions":
                 data = self.db.ExecuteQuery(self.get_sql_actions(i))
+            elif metric == "opened":
+                data = self.db.ExecuteQuery(self.get_sql_opened(i))
+            elif metric == "closed":
+                data = self.db.ExecuteQuery(self.get_sql_closed(i))
+            elif metric == "sent":
+                data = self.db.ExecuteQuery(self.get_sql_sent(i))
             self.check_array_values(data)
             activity = self.add_companies_data (activity, data)
 
-    def result(self, data_source, destdir = None):
+    def result(self, data_source = None, destdir = None):
         if data_source != SCM or destdir is None: return None
 
         automator = Report.get_config()
+        db_identities = automator["generic"]["db_identities"]
+        dbuser = automator["generic"]["db_user"]
+        dbpass = automator["generic"]["db_password"]
+
         start_date = automator['r']['start_date']
         if 'end_date' not in automator['r']:
             end_date = time.strftime('%Y-%m-%d')
@@ -159,19 +245,44 @@ class CompaniesActivity(Analyses):
 
         # First activity should include all companies name
         activity = self.db.ExecuteQuery(self.get_sql_commits())
+        # Commits
         self.add_metric_years("commits",activity,start_year,end_year)
         # Authors
         data = self.db.ExecuteQuery(self.get_sql_authors())
         activity = self.add_companies_data (activity, data)
         self.add_metric_years("authors",activity,start_year,end_year)
-
         # Actions
         data = self.db.ExecuteQuery(self.get_sql_actions())
         activity = self.add_companies_data (activity, data)
         self.add_metric_years("actions",activity,start_year,end_year)
 
+        # We need to change the db to tickets
+        dbname = automator["generic"]["db_bicho"]
+        dsquery = ITS.get_query_builder()
+        dbcon = dsquery(dbuser, dbpass, dbname, db_identities)
+        self.db = dbcon
+        # Tickets opened
+        data = self.db.ExecuteQuery(self.get_sql_opened())
+        activity = self.add_companies_data (activity, data)
+        self.add_metric_years("opened",activity,start_year,end_year)
+        # Tickets closed
+        data = self.db.ExecuteQuery(self.get_sql_closed())
+        activity = self.add_companies_data (activity, data)
+        self.add_metric_years("closed",activity,start_year,end_year)
+
+        # Messages sent 
+        dbname = automator["generic"]["db_mlstats"]
+        dsquery = MLS.get_query_builder()
+        dbcon = dsquery(dbuser, dbpass, dbname, db_identities)
+        self.db = dbcon
+
+        data = self.db.ExecuteQuery(self.get_sql_sent())
+        activity = self.add_companies_data (activity, data)
+        self.add_metric_years("sent",activity,start_year,end_year)
+
+
         # activity = dict (activity.keys() + data.keys())
-        createJSON(activity, destdir+"/scm-companies-activity.json")
+        createJSON(activity, destdir+"/companies-activity.json")
 
     def get_report_files(self, data_source = None):
-        return ["scm-companies-activity.json"]
+        return ["companies-activity.json"]
