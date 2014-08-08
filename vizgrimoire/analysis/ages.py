d3a3f097b4cb05ed5016b3e9763acc389cddf3fb
Jesus M. Gonzalez-Barahona <jgb@gsyc.es>
2014-08-08 14:28:56 +0200

    Aging analysis now seems to work pretty well both with ITS and SCM.

--- vizgrimoire/analysis/ages.py
+++ vizgrimoire/analysis/ages.py
@@ -35,9 +35,26 @@ from jsonpickle import encode, set_encoder_options
 import codecs
 import logging
 
+def produce_json (filename, data, compact = True):
+
+    if compact:
+        # Produce compact JSON output
+        set_encoder_options('json', separators=(',', ': '),
+                            ensure_ascii=False,
+                            encoding="utf8")
+    else:
+        # Produce pretty JSON output
+        set_encoder_options('json', sort_keys=True, indent=4,
+                            separators=(',', ': '),
+                            ensure_ascii=False,
+                            encoding="utf8")
+    data_json = encode(data, unpicklable=False)
+    with codecs.open(filename, "w", "utf-8") as file:
+        file.write(data_json)
+
 class Ages(Analyses):
 
-    id = "ages_scm"
+    id = "ages"
     name = "Ages"
     desc = "Age of developers in project"
 
@@ -45,10 +62,12 @@ class Ages(Analyses):
    
         raise NotImplementedError
 
-
     def result(self, data_source = None):
 
-        logging.info("Producing data for study: Aging_SCM")
+        logging.info("Producing data for study: Aging")
+        if data_source is None:
+            logging.info("Error: no data source for study!")
+            return
         # Prepare the SQLAlchemy database url
         database = 'mysql://' + self.db.user + ':' + \
             self.db.password + '@' + self.db.host + '/' + \
@@ -56,120 +75,93 @@ class Ages(Analyses):
         # Get startdate, endate as datetime objects
         startdate = datetime.strptime(self.filters.startdate, "'%Y-%m-%d'")
         enddate = datetime.strptime(self.filters.enddate, "'%Y-%m-%d'")
-        # Activity data (start time, end time for contributions) for
-        # all the actors, considering only actiivty during
-        # the startdate..enddate period (merges are not considered
-        # as activity)
-        period = PeriodCondition (start = startdate, end = enddate)
-        nomerges = NomergesCondition()
-        data = ActivityPersons (
-            database = database,
-            var = "list_uauthors",
-            conditions = (period,nomerges))
-        # Birth has the ages of all actors, consiering enddate as
-        # current (snapshot) time
-        snapshot = SnapshotCondition (date = enddate)
-        birth = DurationPersons (var = "age",
-                                 conditions = (snapshot,),
-                                 activity = data.activity())
-        # "Aging" has the ages of those actors active during the 
-        # last half year (that is, the period from enddate - half year
-        # to enddate)
-        active_period = ActiveCondition (after = enddate - \
-                                             timedelta(days=182))
-        aging = DurationPersons (var = "age",
-                                 conditions = (snapshot, active_period),
-                                 activity = data.activity())
-        demos = {"birth": birth.durations(),
-                 "aging": aging.durations()}
-        return demos
+        if data_source == SCM:
+            logging.info("Analyzing aging for SCM")
+            # Activity data (start time, end time for contributions) for
+            # all the actors, considering only actiivty during
+            # the startdate..enddate period (merges are not considered
+            # as activity)
+            period = PeriodCondition (start = startdate, end = enddate)
+            nomerges = NomergesCondition()
+            data = ActivityPersons (
+                database = database,
+                var = "list_uauthors",
+                conditions = (period,nomerges))
+            # Birth has the ages of all actors, consiering enddate as
+            # current (snapshot) time
+            snapshot = SnapshotCondition (date = enddate)
+            birth = DurationPersons (var = "age",
+                                     conditions = (snapshot,),
+                                     activity = data.activity())
+            # "Aging" has the ages of those actors active during the 
+            # last half year (that is, the period from enddate - half year
+            # to enddate)
+            active_period = ActiveCondition (after = enddate - \
+                                                 timedelta(days=182))
+            aging = DurationPersons (var = "age",
+                                     conditions = (snapshot, active_period),
+                                     activity = data.activity())
+            demos = {"birth": birth.durations(),
+                     "aging": aging.durations()}
+            return demos
+        if data_source == ITS:
+            logging.info("Analyzing aging for ITS")
+            schema = self.db.database
+            schema_id = self.db.identities_db
+            # Activity data (start time, end time for contributions) for
+            # all the actors, considering only actiivty during
+            # the startdate..enddate period (merges are not considered
+            # as activity)
+            period = its_conditions.PeriodCondition (start = startdate,
+                                                     end = enddate)
+            data = ActivityPersonsITS (
+                database = database,
+                schema = schema, schema_id = schema_id,
+                var = "list_uchangers",
+                conditions = (period,))
+            # Birth has the ages of all actors, consiering enddate as
+            # current (snapshot) time
+            snapshot = SnapshotCondition (date = enddate)
+            birth = DurationPersons (var = "age",
+                                     conditions = (snapshot,),
+                                     activity = data.activity())
+            # "Aging" has the ages of those actors active during the 
+            # last half year (that is, the period from enddate - half year
+            # to enddate)
+            active_period = ActiveCondition (after = enddate - \
+                                                 timedelta(days=182))
+            aging = DurationPersons (var = "age",
+                                     conditions = (snapshot, active_period),
+                                     activity = data.activity())
+            demos = {"birth": birth.durations(),
+                     "aging": aging.durations()}
+            return demos
+        logging.info("Error: data_source not supported!")
+        return {"birth": {},
+                "aging": {}}
 
     def create_report(self, data_source, destdir):
 
-        logging.info("Producing report for study: Aging_SCM")
-        if data_source != SCM: return
+        logging.info("Producing report for study: Aging")
+        if data_source == SCM:
+            prefix = "scm"
+        elif data_source == ITS:
+            prefix = "its"
+        else:
+            logging.info("Error: data_source not supported for Aging")
+            return
         demos = self.result(data_source)
-        # Produce pretty JSON output
-        set_encoder_options('json', sort_keys=True, indent=4,
-                            separators=(',', ': '),
-                            ensure_ascii=False,
-                            encoding="utf8")
-        birth_json = encode(demos["birth"], unpicklable=False)
-        aging_json = encode(demos["aging"], unpicklable=False)
-        with codecs.open(destdir + "/scm-demographics-birth.json", "w", "utf-8") as file:
-            file.write(birth_json)
-        with codecs.open(destdir + "/scm-demographics-aging.json", "w", "utf-8") as file:
-            file.write(aging_json)
-
-class AgesITS(Ages):
-
-    id = "ages_its"
-    name = "Ages ITS"
-    desc = "Age of developers in ITS repositories"
-
-    def result(self, data_source = None):
-
-        logging.info("Producing data for study: Aging_ITS")
-        # Prepare the SQLAlchemy database url
-        database = 'mysql://' + self.db.user + ':' + \
-            self.db.password + '@' + self.db.host + '/'
-        schema = self.db.database
-        schema_id = self.db.identities_db
-        # Get startdate, endate as datetime objects
-        startdate = datetime.strptime(self.filters.startdate, "'%Y-%m-%d'")
-        enddate = datetime.strptime(self.filters.enddate, "'%Y-%m-%d'")
-        # Activity data (start time, end time for contributions) for
-        # all the actors, considering only actiivty during
-        # the startdate..enddate period (merges are not considered
-        # as activity)
-        period = its_conditions.PeriodCondition (start = startdate,
-                                                 end = enddate)
-        data = ActivityPersonsITS (
-            database = database,
-            schema = schema, schema_id = schema_id,
-            var = "list_changers",
-            conditions = (period,))
-        # Birth has the ages of all actors, consiering enddate as
-        # current (snapshot) time
-        snapshot = SnapshotCondition (date = enddate)
-        birth = DurationPersons (var = "age",
-                                 conditions = (snapshot,),
-                                 activity = data.activity())
-        # "Aging" has the ages of those actors active during the 
-        # last half year (that is, the period from enddate - half year
-        # to enddate)
-        active_period = ActiveCondition (after = enddate - \
-                                             timedelta(days=182))
-        aging = DurationPersons (var = "age",
-                                 conditions = (snapshot, active_period),
-                                 activity = data.activity())
-        demos = {"birth": birth.durations(),
-                 "aging": aging.durations()}
-        return demos
-
-    def create_report(self, data_source, destdir):
-
-        logging.info("Producing report for study: Aging_ITS")
-        if not isinstance(data_source,ITS): return
-        demos = self.result(data_source)
-        # Produce pretty JSON output
-        set_encoder_options('json', sort_keys=True, indent=4,
-                            separators=(',', ': '),
-                            ensure_ascii=False,
-                            encoding="utf8")
-        birth_json = encode(demos["birth"], unpicklable=False)
-        aging_json = encode(demos["aging"], unpicklable=False)
-        with codecs.open(destdir + "/its-demographics-birth.json", "w", "utf-8") as file:
-            file.write(birth_json)
-        with codecs.open(destdir + "/its-demographics-aging.json", "w", "utf-8") as file:
-            file.write(aging_json)
+        produce_json (destdir + "/" + prefix + "-demographics-birth.json",
+                      demos["birth"])
+        produce_json (destdir + "/" + prefix + "-demographics-aging.json",
+                      demos["aging"])
+        logging.info("Producing report for study: Aging (done!)")
 
 
 if __name__ == '__main__':
 
     from query_builder import DSQuery
     from metrics_filter import MetricFilters
-    from jsonpickle import encode, set_encoder_options
 
     # Get 
     filters = MetricFilters("months", "'2013-06-01'", "'2014-01-01'", [])
@@ -183,21 +175,21 @@ if __name__ == '__main__':
                         separators=(',', ': '),
                         ensure_ascii=False,
                         encoding="utf8")
-    print encode(ages.result(), unpicklable=False)
+    print encode(ages.result(SCM), unpicklable=False)
 
     # Produce compact JSON output
     set_encoder_options('json', separators=(',', ': '),
                         ensure_ascii=False,
                         encoding="utf8")
-    print encode(ages.result(), unpicklable=False)
+    print encode(ages.result(SCM), unpicklable=False)
 
     dbcon = DSQuery(user = "jgb", password = "XXX", 
                     database = "vizgrimoire_bicho",
                     identities_db = "vizgrimoire_cvsanaly")
-    ages = AgesITS(dbcon, filters)
+    ages = Ages(dbcon, filters)
     # Produce pretty JSON output
     set_encoder_options('json', sort_keys=True, indent=4,
                         separators=(',', ': '),
                         ensure_ascii=False,
                         encoding="utf8")
-    print encode(ages.result(), unpicklable=False)
+    print encode(ages.result(ITS), unpicklable=False)
