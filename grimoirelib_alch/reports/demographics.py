574ec062ab8b66e6586e6cf626589f16ddd0b206
Jesus M. Gonzalez-Barahona <jgb@gsyc.es>
2014-09-08 10:11:39 +0200

    Some common code for reports now in a separate module.

--- grimoirelib_alch/reports/demographics.py
+++ grimoirelib_alch/reports/demographics.py
@@ -24,79 +24,9 @@
 ##   Jesus M. Gonzalez-Barahona <jgb@bitergia.com>
 ##
 
-from os.path import join
-from jsonpickle import encode, set_encoder_options
-import codecs
-
-def produce_json (filename, data, compact = True):
-    """Produce JSON content (data) into a file (filename).
-
-    Parameters
-    ----------
-
-    filename: string
-       Name of file to write the content.
-    data: any
-       Content to write in JSON format. It has to be ready to pack using
-       jsonpickle.encode.
-
-    """
-
-    if compact:
-        # Produce compact JSON output
-        set_encoder_options('json', separators=(',', ': '),
-                            ensure_ascii=False,
-                            encoding="utf8")
-    else:
-        # Produce pretty JSON output
-        set_encoder_options('json', sort_keys=True, indent=4,
-                            separators=(',', ': '),
-                            ensure_ascii=False,
-                            encoding="utf8")
-    data_json = encode(data, unpicklable=False)
-    with codecs.open(filename, "w", "utf-8") as file:
-        file.write(data_json)
-
-
-def create_report (report_files, destdir):
-    """Create report, by producing a collection of JSON files
-
-    Parameters
-    ----------
-
-    report_files: dictionary
-       Keys are the names of JSON files to produce, values are the
-       data to include in those JSON files.
-    destdir: str
-       Name of the destination directory to write all JSON files
-
-    """
-
-    for file in report_files:
-        print "Producing file: ", join (destdir, file)
-        produce_json (join (destdir, file), report_files[file])
-
-def add_report (report, to_add):
-    """Add new files (with their data) to report.
-
-    Adds new_files (which is a dictorionay in report format)
-    to report, and returns the result.
-
-    Parameters
-    ----------
-
-    report: dictionary
-       Base report Keys are the names of JSON files to produce, values are the
-       data to include in those JSON files.
-    to_add: dictionary
-       Report to add. Same format as report. Keys in to_add will
-       replace the same keys in report, or just be added to it.
-    """
-
-    for file in to_add:
-        report[file] = to_add[file]
-    return report
-
+# from os.path import join
+# from jsonpickle import encode, set_encoder_options
+# import codecs
 
 from grimoirelib_alch.family.duration_persons import DurationPersons
 from grimoirelib_alch.family.duration_persons import SnapshotCondition, ActiveCondition
@@ -165,6 +95,8 @@ if __name__ == "__main__":
         ITSActivityPersons,
         MLSActivityPersons
         )
+    from grimoirelib_alch.aux.reports import create_report, add_report
+
     stdout_utf8()
 
     snapshot_date = datetime(2014,7,1)
