375ed33f510fcbd096ba2ff20e5e75327f09404d
Jesus M. Gonzalez-Barahona <jgb@gsyc.es>
2015-03-11 14:33:32 +0100

    Added (very simple) support for ITS() in family.its. Added some simple tests for family.its.

--- grimoirelib_alch/family/its.py
+++ grimoirelib_alch/family/its.py
@@ -23,8 +23,77 @@
 ##   Jesus M. Gonzalez-Barahona <jgb@bitergia.com>
 ##
 
-from common import DBFamily, DBCondition
+from common import DBFamily, DBCondition, Entities, Entity
 from grimoirelib_alch.query.its import DB, Query
+
+class Persons (Entity):
+    """Number of persons
+
+    """
+
+    name = "npersons"
+    desc = "Number of persons in ITS"
+    longdesc = "Number of persons in issue tracking system"
+
+    @staticmethod
+    def query (q):
+
+        return q.select_personsdata_uid("openers").distinct()
+
+
+class ITS (DBFamily):
+    """Constructor of entities in the ITS family.
+
+    This class can be used to instantiate entities from the ITS
+    fammily: direct entities obtained querying a Bicho database.
+
+    The entity to be instantiated is specified using its name, and
+    a set of zero or more conditions that provide context.
+
+    This class provides functions to return different kinds of aggregation
+    (timeseries, total, list) and selection (dates, etc.).
+
+    Valid entity names: "npersons".
+
+    """
+
+    def _produce_query (self, name):
+        """Produce the base query to obtain activity per person.
+
+        This function assumes that self.query was already initialized.
+        The produced query replaces self.query.
+        Conditions will be applied (as filters) later to the query
+        produced by this function.
+
+        Parameters
+        ----------
+        
+        name: {"npersons"}
+           Entity name.
+
+        """
+
+        if name in Entities.subclasses:
+            self.query = Entities.subclasses[name].query(self.query)
+        else:
+            raise Exception ("SCM: Invalid entity name for this family, " + \
+                                 name)
+
+    def timeseries (self):
+        """Return a timeseries for the entity"""
+
+        return self.query.count().group_by_period().timeseries()
+
+    def total (self):
+        """Return the total count for the entity"""
+
+        return self.query.count()
+
+    def list (self, limit = 10):
+        """Return a list for the specified entity"""
+
+        return self.query.limit(limit).all()
+
 
 class PeriodCondition (DBCondition):
     """Period Condition for qualifying a variable
@@ -54,6 +123,48 @@ class PeriodCondition (DBCondition):
         self.end = end
         self.date = date
 
+class OrgsCondition (DBCondition):
+    """Organization Condition for qualifying an entity.
+
+    Specifies the organizations of interest: only their activity will
+    be considered.
+    """
+
+    def __init__ (self, orgs, actors = "changers"):
+        """Instatiation of the object.
+
+        Parameters
+        ----------
+
+        orgs: list of str
+           List of organziations of interest 
+        actors: {"authors", "committers"}
+            Kind of actors to consider
+
+        """
+
+        self.org_names = orgs
+        self.actors = actors
+
+    def filter (self, query):
+        """Filter to apply for this condition
+
+        Parameters
+        ----------
+
+        query: Query
+           Query to which the filter will be applied
+
+        """
+
+        # Get the session for this query, use it for getting org ids,
+        # and build the filter
+        session = inspect(query).session
+        query_orgs = session.query().select_orgs() \
+            .filter_orgs (self.org_names)
+        self.org_ids = [org.org_id for org in query_orgs.all()]
+        return query.filter_org_ids(list = self.org_ids,
+                                    kind = self.actors)
 
 if __name__ == "__main__":
 
--- grimoirelib_alch/query/its.py
+++ grimoirelib_alch/query/its.py
@@ -50,7 +50,7 @@ class DB (GrimoireDatabase):
 
         return Query
 
-    def _create_tables(self):
+    def _create_tables(self, tables = None, tables_id = None):
         """Create all SQLAlchemy tables.
 
         Builds a SQLAlchemy class per SQL table, by using _table().
@@ -97,6 +97,19 @@ class DB (GrimoireDatabase):
             bases = (self.Base,), name = 'UPeople',
             tablename = 'upeople',
             schemaname = self.schema_id)
+        DB.Companies = GrimoireDatabase._table (
+            bases = (self.Base,), name = 'Companies',
+            tablename = 'companies',
+            schemaname = self.schema_id,
+            columns = dict (
+                id = Column(Integer, primary_key = True)
+                )
+            )
+        DB.UPeopleCompanies = GrimoireDatabase._table (
+            bases = (self.Base,), name = 'UPeopleCompanies',
+            tablename = 'upeople_companies',
+            schemaname = self.schema_id,
+            )
 
 
 class Query (GrimoireQuery):
@@ -243,6 +256,25 @@ class Query (GrimoireQuery):
         return query
 
 
+    def select_orgs (self):
+        """Select organizations data.
+
+        Include id and name, as they appear in the companies table.
+        Warning: doesn't join other tables. For now, only works alone.
+
+        Returns
+        -------
+
+        Query
+            Including new columns in SELECT
+        
+        """
+
+        query = self.add_columns (label("org_id", DB.Companies.id),
+                                  label("org_name", DB.Companies.name))
+        return query
+
+
     def filter_period(self, start = None, end = None, date = "change"):
         """Filter variable for a period
 
@@ -267,6 +299,63 @@ class Query (GrimoireQuery):
         if end is not None:
             self.end = end
             query = query.filter(date_field < end.isoformat())
+        return query
+
+
+    def filter_orgs (self, orgs):
+        """Filter organizations matching a list of names
+
+        Fiters query by a list of organization names, checking for
+        them in the companies table.
+
+        Parameters
+        ----------
+        
+        orgs: list of str
+            List of organizations
+
+        """
+
+        query = self
+        query = query.filter(DB.Companies.name.in_(orgs))
+        return query
+
+
+    def filter_org_ids (self, list, kind):
+        """Filter organizations matching a list of organization ids
+
+        Fiters query by a list of organization ids.
+
+        Parameters
+        ----------
+        
+        list: list of int
+            List of organization ids
+        kind: {"openers", "closers", "changers"}
+           Kind of person to select
+
+        """
+
+        query = self
+        if kind == "changers":
+            person_id = DB.Changes.changed_by
+            date_id = DB.Changes.changed_on
+        elif kind in ("closers", "openers"):
+            raise Exception ("filter_org_ids: " \
+                                 "Kind not yet implemented %s." \
+                                 % kind)
+        else:
+            raise Exception ("filter_org_ids: Unknown kind %s." \
+                                 % kind)
+        query = query \
+            .join (DB.UPeopleCompanies,
+                   DB.PeopleUPeople.upeople_id == \
+                       DB.UPeopleCompanies.upeople_id) \
+            .filter (date_id.between (
+                           DB.UPeopleCompanies.init,
+                           DB.UPeopleCompanies.end
+                           )) \
+            .filter (DB.UPeopleCompanies.company_id.in_(list))
         return query
 
 
