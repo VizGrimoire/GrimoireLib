7543a95f4e302326cdc2f02f81ea16e2e568286d
Alvaro del Castillo <acs@bitergia.com>
2015-03-26 10:14:36 +0100

    Removed SCM newauthors metric. Too slow for repositories. In final version should be done in automator config for scm metrics.

--- vizgrimoire/SCM.py
+++ vizgrimoire/SCM.py
@@ -552,19 +552,19 @@ class SCM(DataSource):
     @staticmethod
     def get_metrics_core_agg():
         m  = ['commits','authors','committers','branches','files','actions']
-        m += ['added_lines','removed_lines', 'repositories','newauthors']
+        m += ['added_lines','removed_lines', 'repositories']
         m += ['avg_commits', 'avg_files', 'avg_commits_author', 'avg_files_author']
         return m
 
     @staticmethod
     def get_metrics_core_ts():
         m  = ['commits','authors','committers','branches','files']
-        m += ['added_lines','removed_lines','repositories','newauthors']
+        m += ['added_lines','removed_lines','repositories']
         return m
 
     @staticmethod
     def get_metrics_core_trends():
-        return ['commits','authors','files','lines','newauthors']
+        return ['commits','authors','files','lines']
 
 #
 # People
