2e3aca48f7a4c50eb15f1eca7334c5bd1c12f5ef
Alvaro del Castillo <acs@bitergia.com>
2015-04-15 12:27:14 +0200

    [report_tool] Use GROUP BY in SCR also for all filters supported.

--- vizGrimoireJS/report_tool.py
+++ vizGrimoireJS/report_tool.py
@@ -93,7 +93,7 @@ def create_reports_filters(period, startdate, enddate, destdir, npeople, identit
                          "its":["people2","company","company+country","country","repository","domain"],
                          "its_1":[],
                          "mls":[],
-                         "scr":[],
+                         "scr":["people2","country","company","repository"],
                          "mediawiki":[],
                          "irc":[],
                          "downloads":[],
